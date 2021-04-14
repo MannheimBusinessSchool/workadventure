5cf46b88f571191bac4de184492257ad5ae9b149
Folke Gleumes <folke@gleumes.org>
2021-04-14 13:05:43 +0200

    prepare Dockerfile for push to S3

---
+++ front/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# exit immediately when a command fails
+set -e
+
+# abort on attempting to use an undefined variable (hello Valve! :))
+set -u
+
+# set exitcode of a pipe chain to the exit code of the rightmost
+# command not exiting with zero, if there are any
+# in combination with -e this aborts on failure within a pipe chain
+set -o pipefail
+
+# make functions inherit ERR-traps, so that they fire when set -e takes effect there
+set -E
+
+aws configure
+./templater.sh
+yarn run build
+aws s3 sync dist "$S3_BUCKET" --delete --acl public-read
